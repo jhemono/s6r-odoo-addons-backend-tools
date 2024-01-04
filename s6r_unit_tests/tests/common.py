2553b2c58406c9291565e5b8e2b672e5cd30fb82
Michel Perrocheau <michel@scalizer.fr>
2024-01-04 12:33:45 +0100

    [IMP] use formatted message in _handle_console overriding

--- s6r_unit_tests/tests/common.py
+++ s6r_unit_tests/tests/common.py
@@ -1,6 +1,7 @@
 # Copyright 2023 Scalizer (<https://www.scalizer.fr>)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-
+import re
+import copy
 from odoo.tests.common import ChromeBrowser
 _handle_console_origin = ChromeBrowser._handle_console
 
@@ -19,12 +20,26 @@ def get_console_message_to_skip(self):
 ChromeBrowser.get_console_message_to_skip = get_console_message_to_skip
 
 
+def get_formatted_message(self, args):
+    """ This is just a factorization of the beginning of the original v16 _handle_console function """
+    if args:
+        arg0, args = str(self._from_remoteobject(args[0])), args[1:]
+    else:
+        arg0, args = '', []
+    formatted = [re.sub(r'%[%sdfoOc]', self.console_formatter(args), arg0)]
+    formatted.extend(str(self._from_remoteobject(arg)) for arg in args)
+    return ' '.join(formatted)
+
+
+ChromeBrowser.get_formatted_message = get_formatted_message
+
+
 def _handle_console(self, type, args=None, stackTrace=None, **kw):
-    if args and isinstance(args[0], dict) and args[0].get('value'):
-        for msg_to_skip in self.get_console_message_to_skip():
-            if msg_to_skip['type'] == type:
-                if msg_to_skip['msg'] in args[0].get('value'):
-                    return
+    for msg_to_skip in self.get_console_message_to_skip():
+        if msg_to_skip['type'] == type:
+            msg = self.get_formatted_message(copy.deepcopy(args))
+            if msg_to_skip['msg'] in msg:
+                return
     _handle_console_origin(self, type, args, stackTrace, **kw)
 
 
